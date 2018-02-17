2ea5e49049ddc6db9079ec69d1e469cdab6affb3
Kees C. Bakker <c.bakker@gmx.net>
2018-02-17 19:22:25 +0100

    Documentation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hubot-command-mapper
-Helps with mapping tools and commands to Hubot.
+Help with the creation of tools and command for hubot.
+
+A _tool_ is a collection of _command_ objects that can be invoked. A convention is used to map the command into hubot. The direct question `@hubot clear screen` will launch the `screen` command of the `clear` tool.
+
+## A simple example
+Let's define the clear screen command by replying with 48 space-line replies.
--- src/definitions/itool.d.ts
+++ src/definitions/itool.d.ts
@@ -1,8 +1,16 @@
 interface ITool
 {
+    /** Name of the tool. A required property. */
     name: string;
+
+    /** The command that are supported by the tool.
+     *  Only tools with at least 1 command can be mapped. */
+    commands: Array<ICommand>;
+    
+    /** Used for user-name based authorization. Only the specificed 
+     * users may access the tool. */
     auth?: Array<string>;
+
     mute?: boolean;
     registrations?: Array<{commandName: string, messageRegex: string}>;
-    commands: Array<ICommand>;
 }
