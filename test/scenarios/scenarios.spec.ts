2fabf34a9a8a022d4f639ed7ac76c5c95b83d4e1
Kees C. Bakker <c.bakker@gmx.net>
2018-10-28 15:04:56 +0100

    Moved scenarios around.

--- test/scenarios/scenarios.spec.ts
+++ test/scenarios/scenarios.spec.ts
@@ -1,13 +1,8 @@
 const pretend = require("hubot-pretend");
 
-import { mapper, Options, NumberParameter } from "./../src/";
+import { mapper, Options, NumberParameter, RegExStringParameter, TokenParameter, IPv4Parameter } from "../../src";
 import { expect } from "chai";
 import "mocha";
-import {
-  IPv4Parameter,
-  TokenParameter,
-  RegExStringParameter
-} from "../src/";
 
 describe("scenarios.spec.ts > wehkamp glitch", () => {
   beforeEach(() => {
