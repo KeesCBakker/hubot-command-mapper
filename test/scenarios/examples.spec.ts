d251cb0bc3bdac2c087292a4f63e120330b65e42
Kees C. Bakker <c.bakker@gmx.net>
2018-11-20 12:14:15 +0100

    Added examples as tests.

---
+++ test/scenarios/examples.spec.ts
@@ -0,0 +1,112 @@
+const pretend = require("hubot-pretend");
+
+import { Options } from "./../../src/";
+import { expect } from "chai";
+import "mocha";
+import { mapper, StringParameter } from "./../../src/";
+
+describe("examples.spec.ts > check count/capture example", () => {
+
+    it("Should count to 3", done => {
+
+        pretend.start({
+            name: "hb",
+            alias: "lb"
+        });
+
+        var options = new Options();
+        options.verbose = false;
+
+        const tool = {
+            name: "count",
+            commands: [{
+                name: "from",
+                capture: "(\\d+) to (\\d+)",
+                execute: context => {
+                    const a = Number(context.match[context.match.length - 2])
+                    const b = Number(context.match[context.match.length - 1])
+
+                    for (let i = a; i < b + 1; i++) {
+                        context.res.reply(`${i}!`)
+                    }
+                }
+            }]
+        };
+
+        mapper(pretend.robot, tool, options);
+
+        pretend
+            .user("kees")
+            .send("@hb count from 1 to 3")
+            .then(() => {
+
+                expect(pretend.messages).to.eql(
+                    [
+                        ['kees', '@hb count from 1 to 3'],
+                        ['hubot', '@kees 1!'],
+                        ['hubot', '@kees 2!'],
+                        ['hubot', '@kees 3!']
+                    ]
+                )
+
+                pretend.shutdown();
+                done();
+            })
+            .catch(ex => done(ex));
+
+    });
+});
+
+
+describe("examples.spec.ts > check norris impersonate / parameter", () => {
+
+    it("Should return a quote", done => {
+
+        pretend.start({
+            name: "hb",
+            alias: "lb"
+        });
+
+        var options = new Options();
+        options.verbose = false;
+
+        const tool = {
+            name: "norris",
+            commands: [{
+                name: 'impersonate',
+                parameters: [new StringParameter('firstName'), new StringParameter('lastName')],
+                execute: context => {
+                    const firstName = encodeURIComponent(context.values.firstName)
+                    const lastName = encodeURIComponent(context.values.lastName)
+
+                    context.res.reply(`${firstName} ${lastName} has counted to infinity. Twice!`)
+                }
+            }]
+        };
+
+        mapper(pretend.robot, tool, options);
+
+        pretend
+            .user("kees")
+            .send("@hb norris impersonate Cool Cat")
+            .then(() => {
+
+                expect(pretend.messages).to.eql(
+                    [
+                        ['kees', '@hb norris impersonate Cool Cat'],
+                        ['hubot', '@kees Cool Cat has counted to infinity. Twice!']
+                    ]
+                )
+
+                pretend.shutdown();
+                done();
+            })
+            .catch(ex => done(ex));
+
+    });
+});
+
+
+
+
+
