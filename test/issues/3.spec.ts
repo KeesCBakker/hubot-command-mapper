63cd531a878bcac03424396d282ad96056b82115
Kees C. Bakker <c.bakker@gmx.net>
2018-11-20 05:31:36 +0100

    Unit test for issue 3 - the alias was not being used by the command mapper.

---
+++ test/issues/3.spec.ts
@@ -0,0 +1,56 @@
+const pretend = require("hubot-pretend");
+
+import { map_command, Options, alias } from "./../../src";
+import { expect } from "chai";
+import "mocha";
+
+describe("issues / 3.spec.ts / Testing problems with robot not responding to alias.", () => {
+
+  beforeEach(() => {
+
+    pretend.start({
+      name: 'namebot',
+      alias:'aliasbot'
+    });
+
+    var options = new Options();
+    options.verbose = false;
+
+    map_command(pretend.robot, "ping", options, (context) => context.res.reply("pong"));
+    alias(pretend.robot, { "pang": "ping" }, options);
+
+  });
+
+  afterEach(() => pretend.shutdown());
+
+  it("Should respond to the alias and execute the command", done => {
+    pretend
+      .user("kees")
+      .send("@aliasbot ping")
+      .then(() => {
+        console.log(pretend.messages);
+        expect(pretend.messages).to.eql([
+          ["kees", "@aliasbot ping"],
+          ["hubot", "@kees pong"]
+        ]);
+        done();
+      })
+      .catch(ex => done(ex));
+  });
+
+  it("Should respond to the alias and execute the command alias", done => {
+    pretend
+      .user("kees")
+      .send("@aliasbot pang")
+      .then(() => {
+        console.log(pretend.messages);
+        expect(pretend.messages).to.eql([
+          ["kees", "@aliasbot pang"],
+          ["hubot", "@kees pong"]
+        ]);
+        done();
+      })
+      .catch(ex => done(ex));
+  });
+
+});
